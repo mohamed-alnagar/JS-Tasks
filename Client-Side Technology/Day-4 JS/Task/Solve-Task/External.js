938fd96dc109f49d144ac19d8042536507f2c589
mohamed-alnagar <mohamedalnagar20@gmail.com>
2025-08-26 00:08:26 +0300

    Initial commit

---
+++ Client-Side Technology/Day-4 JS/Task/Solve-Task/External.js
@@ -0,0 +1,37 @@
+
+// console.log("Number1 = " , number1);
+// console.log("Number2 = " , number2);
+// console.log("Number3 = " , number3);
+
+
+number1= 3 ;
+number2=2.9 ;
+number3=0xff;
+
+var firstName="Mohamed" ;
+var middleName='Abdelnaby';
+var lastName=`Alnagar` ;
+
+var flag =true;
+
+console.log("This is the External JS File");
+console.log("number1 :", number1);
+console.log("number2 :", number2);
+console.log("number3 :", number3);
+console.log("firstName", firstName);
+console.log("middleName", middleName);
+console.log("lastName", lastName);
+console.log("flag", flag);
+
+
+console.log("typeof number1:", typeof number1);
+console.log("typeof number2:", typeof number2);
+console.log("typeof number3:", typeof number3);
+console.log("typeof firstName:", typeof firstName);
+console.log("typeof middleName:", typeof middleName);
+console.log("typeof lastName:", typeof lastName);
+console.log("typeof flag:", typeof flag);
+
+
+
+
